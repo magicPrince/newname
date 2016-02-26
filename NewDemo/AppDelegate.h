04f02cb8786d8229188504c7ac1a0c4157ae93b0
Slahser <962958179@qq.com>
2016-02-26 15:44:29 +0800

    Initial Commit

---
+++ NewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDemo
+//
+//  Created by   caoyf on 16/2/26.
+//  Copyright © 2016年 niit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewDemo
+//
+//  Created by   caoyf on 16/2/26.
+//  Copyright © 2016年 niit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
